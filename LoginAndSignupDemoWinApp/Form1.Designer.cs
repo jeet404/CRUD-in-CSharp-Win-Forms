1746574a69d972902c0e3258a23f17e9ba14455c
jeet404 <jeet63240@gmail.com>
2022-03-10 19:18:48 +0530

    Add Files.

---
+++ LoginAndSignupDemoWinApp/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace LoginAndSignupDemoWinApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_login = new System.Windows.Forms.Button();
+            this.btn_regi = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_unm = new System.Windows.Forms.TextBox();
+            this.txt_pass = new System.Windows.Forms.TextBox();
+            this.lbl_stat = new System.Windows.Forms.Label();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.btn_show = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_login
+            // 
+            this.btn_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_login.Location = new System.Drawing.Point(101, 178);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(124, 33);
+            this.btn_login.TabIndex = 0;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // btn_regi
+            // 
+            this.btn_regi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_regi.Location = new System.Drawing.Point(101, 217);
+            this.btn_regi.Name = "btn_regi";
+            this.btn_regi.Size = new System.Drawing.Size(124, 33);
+            this.btn_regi.TabIndex = 1;
+            this.btn_regi.Text = "Register";
+            this.btn_regi.UseVisualStyleBackColor = true;
+            this.btn_regi.Click += new System.EventHandler(this.btn_regi_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(83, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(173, 33);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Login Form";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txt_unm
+            // 
+            this.txt_unm.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_unm.Location = new System.Drawing.Point(120, 98);
+            this.txt_unm.Name = "txt_unm";
+            this.txt_unm.Size = new System.Drawing.Size(194, 26);
+            this.txt_unm.TabIndex = 5;
+            // 
+            // txt_pass
+            // 
+            this.txt_pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_pass.Location = new System.Drawing.Point(120, 130);
+            this.txt_pass.Name = "txt_pass";
+            this.txt_pass.PasswordChar = '*';
+            this.txt_pass.Size = new System.Drawing.Size(194, 26);
+            this.txt_pass.TabIndex = 6;
+            // 
+            // lbl_stat
+            // 
+            this.lbl_stat.AutoSize = true;
+            this.lbl_stat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_stat.Location = new System.Drawing.Point(130, 349);
+            this.lbl_stat.Name = "lbl_stat";
+            this.lbl_stat.Size = new System.Drawing.Size(59, 20);
+            this.lbl_stat.TabIndex = 7;
+            this.lbl_stat.Text = "          ";
+            this.lbl_stat.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_update
+            // 
+            this.btn_update.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_update.Location = new System.Drawing.Point(101, 256);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(124, 33);
+            this.btn_update.TabIndex = 8;
+            this.btn_update.Text = "Update Data";
+            this.btn_update.UseVisualStyleBackColor = true;
+            this.btn_update.Click += new System.EventHandler(this.btn_update_Click);
+            // 
+            // btn_show
+            // 
+            this.btn_show.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_show.Location = new System.Drawing.Point(101, 295);
+            this.btn_show.Name = "btn_show";
+            this.btn_show.Size = new System.Drawing.Size(124, 33);
+            this.btn_show.TabIndex = 9;
+            this.btn_show.Text = "Show Data";
+            this.btn_show.UseVisualStyleBackColor = true;
+            this.btn_show.Click += new System.EventHandler(this.btn_show_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 386);
+            this.Controls.Add(this.btn_show);
+            this.Controls.Add(this.btn_update);
+            this.Controls.Add(this.lbl_stat);
+            this.Controls.Add(this.txt_pass);
+            this.Controls.Add(this.txt_unm);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_regi);
+            this.Controls.Add(this.btn_login);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Button btn_regi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_unm;
+        private System.Windows.Forms.TextBox txt_pass;
+        private System.Windows.Forms.Label lbl_stat;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.Button btn_show;
+    }
+}
+
